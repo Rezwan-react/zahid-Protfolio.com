08a06dc0f4b2652ed180b437923d100534ce1675
Rezwan-react <mdrazwanislam8@gmail.com>
2025-11-08 20:03:43 +0600

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import HomeComponents from '../Components/home/HomeComponents'
+import About from './About'
+import Skills from './Skills'
+import Projects from './Projects'
+import ExperienceComponents from '../Components/Experience/ExperienceComponents'
+import AchievementsComponents from '../Components/Achievements/AchievementsComponents'
+import ContactComponents from '../Components/Contact/ContactComponents'
+
+const Home = () => {
+  return (
+    <>
+      <HomeComponents />
+      <About />
+      <Skills />
+      <Projects />
+      <ExperienceComponents />
+      <AchievementsComponents />
+      <ContactComponents />
+    </>
+  )
+}
+
+export default Home
